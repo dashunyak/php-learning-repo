827378a6cd3e0da0f2e31f3b0484f1fa44b78e64
dashunyak <128722772+dashunyak@users.noreply.github.com>
2023-04-05 22:51:31 +0500

    Update 4.php

---
+++ tasks/Копылова Дарья/2.11/4.php
@@ -0,0 +1,8 @@
+<?php
+    $str = '123321';
+    $res1 = $str[0] + $str[1] + $str[2];
+    $res2 = $str[3] + $str[4] + $str[5];
+    if ($res1 == "3" and $res2 == "3") {
+        echo "да";
+    }
+?>
