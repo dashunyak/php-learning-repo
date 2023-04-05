85342a6b2e46998f34c31492d71b948eae1a1462
dashunyak <128722772+dashunyak@users.noreply.github.com>
2023-04-05 22:50:09 +0500

    Update 1.php

---
+++ tasks/Копылова Дарья/2.11/1.php
@@ -0,0 +1,12 @@
+<?php
+    $month = 3;
+    if ($month == 12 or $month == 1 or $month == 2) {
+        echo "зима";
+    } elseif ($month == 3 or $month == 4 or $month == 5) {
+        echo "весна";
+    } elseif ($month == 6 or $month == 7 or $month == 8) {
+        echo "лето";
+    } elseif ($month == 9 or $month == 10 or $month == 11) {
+        echo "осень";
+    } 
+?>
