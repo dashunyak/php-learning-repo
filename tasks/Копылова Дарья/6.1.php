9b3e6fe836e5b646c31b92748b2351f96279cf6f
dashunyak <128722772+dashunyak@users.noreply.github.com>
2023-04-07 19:41:35 +0500

    Update 6.1.php

--- tasks/Копылова Дарья/6.1.php
+++ tasks/Копылова Дарья/6.1.php
@@ -1,15 +1,15 @@
 <?php
-	function func() {
-		echo 'Даша';
-	}
-	func(); 
+    function func() {
+        echo 'Даша';
+    }
+    func(); 
 ?>
 <br>
 <?php
-	function funct() {
-	$arr = range(1, 100);
-    $summ = array_sum($arr);
-    echo $summ;
-	}
-	funct(); 
+    function funct() {
+    $arr = range(1, 100);
+        $summ = array_sum($arr);
+        echo $summ;
+    }
+    funct(); 
 ?>
